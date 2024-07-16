66567ea161661ac8b5bf857745c219a6dc737a3a
Sahil11122 <sahilchalke21@gmail.com>
2024-07-16 12:12:08 +0530

    adding view order freature

--- src/components/MainNav.tsx
+++ src/components/MainNav.tsx
@@ -1,6 +1,7 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import { Button } from "./ui/button"
 import UsernameMenu from "./UserNameMenu";
+import { Link } from "react-router-dom";
 
 const MainNav = () => {
   const {loginWithRedirect, isAuthenticated }= useAuth0();
@@ -8,7 +9,12 @@ const MainNav = () => {
   return(
     <span className="flex space-x-2 items-center">
       {isAuthenticated ? (
+      <>
+        <Link to="/order-status" className="font-bold hover:text-red-500">
+            Order Status
+        </Link>
       <UsernameMenu/>
+      </>
       ) :(
       <Button variant="ghost" 
     className="font-bold hover:text-red-500 hover:bg-white" 
--- src/components/MobileNav.tsx
+++ src/components/MobileNav.tsx
@@ -27,7 +27,7 @@ const MobileNav = () => {
               {user?.email}
             </span>
           ) : (
-            <span> Welcome to MernEats.com!</span>
+            <span> Welcome to Red Hibiscus.com!</span>
           )}
         </SheetTitle>
         <Separator />
